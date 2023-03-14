1346e8bf71e9e51784247a68b64f59cc5aea8085
0739ik <127084933+0739ik@users.noreply.github.com>
2023-03-14 12:01:41 +0000

    example-2-3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,9 +3,31 @@ class MessagesController < ApplicationController
     @user = User.find_by(name: params[:name])
     @message = @user.messages.new(message: params[:message]) 
     if @message.save
-      render json: 'OK'
+      render json: 'Registration successful'
     else
-      render json: 'NG'
+      render json: 'Registration failed'
     end
   end
+
+  def show
+    result_message=nil
+    @messages =Message.joins(:user).where(users: {name: params[:name]})
+    
+    if @messages.length !=0
+      @messages.each do |message|
+        if result_message == nil
+          result_message='"message": ['
+        else
+          result_message += ','
+        end  
+        result_message += ' { "registered_at": "' + message.created_at.to_s + '",'
+        result_message += ' "message": "' + message.message + '"}'
+      end
+      result_message += ']'  
+    else
+      result_message='0件'
+    end
+    render json: result_message
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   post 'user/regist'
+  
+  post 'user/:name/message' => 'messages#show'
   post 'user/:name/message/regist' => 'messages#regist'
   root "hello#index"
 end
