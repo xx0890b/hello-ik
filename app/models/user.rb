51dfd7aefde9efbf780e4dfbe88b004535b4cc17
0739ik <127084933+0739ik@users.noreply.github.com>
2023-03-21 01:08:25 +0000

    example-2-5_0321修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,5 @@
 class User < ApplicationRecord
     has_many:messages
-    def get_user_id_by_name(name)
-        begin
-            user = User.find_by!(name: name)
-            # status = true
-        rescue => exception
-            # status = false
-        end
-        return user.id
-    end
 
     def get_message_by_date_time(date_time)
 
